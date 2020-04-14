86e261f4185a98804b6cfddcc19c3c75493c772d
fujimoto kyosuke <kyoro.f@gmail.com>
2020-04-15 07:58:06 +0900

    Add test

---
+++ main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	for _, test := range tests {
+		in := bytes.NewBufferString(test.src)
+		out := &bytes.Buffer{}
+		run(in, out)
+		if test.want != out.String() {
+			t.Errorf("\nwant:\n%s\ngot:\n%s", test.want, out.String())
+		}
+	}
+}
+
+var tests = []struct {
+	src  string
+	want string
+}{
+	{
+		`foo(aaa=123, bbb="bbbstr", ccc=[10, 20, 30], ddd=bar(eee=null, fff=false))`,
+		`foo(
+  aaa=123,
+  bbb="bbbstr",
+  ccc=[
+    10,
+    20,
+    30
+  ],
+  ddd=bar(
+    eee=null,
+    fff=false
+  )
+)
+`,
+	},
+}
