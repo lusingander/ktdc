8dbef4dc5f2675b5f1d3856da93bccb160d7fa50
fujimoto kyosuke <kyoro.f@gmail.com>
2020-04-14 22:55:26 +0900

    Extract func

--- main.go
+++ main.go
@@ -1,36 +1,11 @@
 package main
 
-import "fmt"
-
-func main() {
-	str := `foo(aaa=123,bbb="bbbstr",ccc=[10,20,30],ddd=bar(eee=null,fff=false))`
-
-	i, p := 0, 0
-	for {
-		c := str[p]
-		if c == ',' {
-			fmt.Println(tab(i) + str[0:p+1])
-			str = str[p+1:]
-			p = 0
-		} else if c == '(' || c == '[' {
-			fmt.Println(tab(i) + str[0:p+1])
-			str = str[p+1:]
-			p = 0
-			i++
-		} else if c == ')' || c == ']' {
-			fmt.Println(tab(i) + str[0:p])
-			str = str[p:]
-			p = 0
-			i--
-			if len(str) == 1 {
-				// data class なので最後は必ず ')' になるはず
-				fmt.Println(str)
-				break
-			}
-		}
-		p++
-	}
-}
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
 func tab(n int) string {
 	s := ""
@@ -38,4 +13,44 @@ func tab(n int) string {
 		s += "  "
 	}
 	return s
+}
+
+func scan(in io.Reader) string {
+	s := bufio.NewScanner(in)
+	s.Scan()
+	return s.Text()
+}
+
+func run(in io.Reader, out io.Writer) {
+	str := scan(in)
+
+	i, p := 0, 0
+	for {
+		c := str[p]
+		if c == ',' {
+			fmt.Fprintln(out, tab(i)+str[0:p+1])
+			str = str[p+1:]
+			p = 0
+		} else if c == '(' || c == '[' {
+			fmt.Fprintln(out, tab(i)+str[0:p+1])
+			str = str[p+1:]
+			p = 0
+			i++
+		} else if c == ')' || c == ']' {
+			fmt.Fprintln(out, tab(i)+str[0:p])
+			str = str[p:]
+			p = 0
+			i--
+			if len(str) == 1 {
+				// data class なので最後は必ず ')' になるはず
+				fmt.Fprintln(out, str)
+				break
+			}
+		}
+		p++
+	}
+}
+
+func main() {
+	run(os.Stdin, os.Stdout)
 }
